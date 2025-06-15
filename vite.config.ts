e8842b3dcb4e0e56ff78d5e79a26e0bf95d83c3e
Harry Moldovanis <hmoldo@gmail.com>
2025-06-15 22:09:16 +0200

    v

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,33 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
 
-// https://vite.dev/config/
+import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
+
 export default defineConfig({
-  plugins: [vue()],
-})
+  plugins: [vue(), cssInjectedByJsPlugin()],
+  resolve: {
+    alias: {
+      "@/": new URL("./src/", import.meta.url).pathname,
+    },
+  },
+
+  build: {
+    cssCodeSplit: true,
+    target: "esnext",
+    lib: {
+      entry: path.resolve(__dirname, "src/index.ts"),
+      name: "GithubPackagesUiLibrary",
+      fileName: (format) => `github-packages-ui-library.${format}.js`,
+    },
+
+    rollupOptions: {
+      external: ["vue"],
+      output: {
+        globals: {
+          vue: "Vue",
+        },
+      },
+    },
+  },
+});
